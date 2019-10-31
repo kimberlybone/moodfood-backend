393cbc58f1764d04fc16e9184bc99e7a74fb8f9d
kimberlybone <k_bone@uncg.edu>
2019-10-31 16:26:40 -0400

    added route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :recipes
   resources :moods
   resources :users
+
+  get '/login', to: 'login#create', as: 'login'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
