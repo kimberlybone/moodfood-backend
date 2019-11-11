fa75857d63a6ebbbd868ad26c5fd7fe9a1f1bafd
kimberlybone <k_bone@uncg.edu>
2019-11-11 14:46:28 -0500

    created custom method for Mood serializer

--- app/serializers/mood_serializer.rb
+++ app/serializers/mood_serializer.rb
@@ -1,3 +1,10 @@
 class MoodSerializer < ActiveModel::Serializer
-  attributes :id, :name
+  attributes :id, :name, :recipes
+
+  def recipes
+    mood_recipes = Mood.all.map do |m|
+      m.recipes
+    end
+    return mood_recipes
+  end
 end
